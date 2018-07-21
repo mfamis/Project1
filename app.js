25e54bcff9516a613098a6c8bb084bda08ebc7c2
Nguyen Le <ucnguyenle@gmail.com>
2018-07-21 14:50:57 -0700

    add geolocations

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var loc = {
 	lat: null,
 	lon: null,
@@ -16,28 +17,37 @@ var loc = {
 	}
 };
 
-$.ajax({
-    method: "GET",
-    crossDomain: true,
-    // count is how many restaurants we want
-    // establishment_type is the type of restaurant we want
-    url: "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + loc.lat + "lon=" + loc.lon + "&radius=2",
-    dataType: "json",
-    async: true,
-    headers: {
-      "user-key": "7e43dd3df445b7aee59f4e05cf1204c7"
-    },
-    success: function(data) {
-        console.log(data);
-      var res = [];
-      res = data.restaurants;
-      for (var j = 0; j < res.length; j++) {
+$().ready(function() {
+    loc.getLocation();
+
+    nearbyLoc();
+    });
+
+function nearbyLoc(){
+    $.ajax({
+        method: "GET",
+        crossDomain: true,
+        // count is how many restaurants we want
+        // establishment_type is the type of restaurant we want
+        url: "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + loc.lat + "lon=" + loc.lon + "&radius=2",
+        dataType: "json",
+        async: true,
+        headers: {
+          "user-key": "7e43dd3df445b7aee59f4e05cf1204c7"
+        },
+        success: function(data) {
+            console.log(data);
+          var res = [];
+          res = data.restaurants;
+          for (var j = 0; j < res.length; j++) {
+        
+              $('.results').append("<div class='name'>" + "Restaurant: " + res[j].restaurant.name + "</div>" + "\n" + "<div class='cuisines'>" + res[j].restaurant.cuisines + "</div>");
+     
+          }
+        },
+        error: function() {
+          $('.results').append("<div>Sorry, data is not coming through. Refresh and try again.</div>");
+        }
+      });
     
-          $('.results').append("<div class='name'>" + "Restaurant: " + res[j].restaurant.name + "</div>" + "\n" + "<div class='cuisines'>" + res[j].restaurant.cuisines + "</div>");
- 
-      }
-    },
-    error: function() {
-      $('.results').append("<div>Sorry, data is not coming through. Refresh and try again.</div>");
-    }
-  });
+}
