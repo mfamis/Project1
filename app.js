5d48117731cd3fc3619246406cc3b0a8e1b750f4
Nguyen Le <ucnguyenle@gmail.com>
2018-07-21 14:35:06 -0700

    add js

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
+var loc = {
+	lat: null,
+	lon: null,
 
-$(document).ready(function(){
-  $('.tabs').tabs();
+	// get user's location if possible
+	getLocation: function() {
+		if ("geolocation" in navigator) {
+			var watchID = navigator.geolocation.watchPosition(function(position) {
+				loc.lat = position.coords.latitude;
+				loc.lon = position.coords.longitude;
 
+				console.log("lat: " + loc.lat);
+				console.log("lon: " + loc.lon);
+			});
+		}
+	}
+};
 
-});
-
-  $('.fixed-action-btn').floatingActionButton({
-    toolbarEnabled: true
+$.ajax({
+    method: "GET",
+    crossDomain: true,
+    // count is how many restaurants we want
+    // establishment_type is the type of restaurant we want
+    url: "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + loc.lat + "lon=" + loc.lon + "&radius=2",
+    dataType: "json",
+    async: true,
+    headers: {
+      "user-key": "7e43dd3df445b7aee59f4e05cf1204c7"
+    },
+    success: function(data) {
+        console.log(data);
+      var res = [];
+      res = data.restaurants;
+      for (var j = 0; j < res.length; j++) {
+    
+          $('.results').append("<div class='name'>" + "Restaurant: " + res[j].restaurant.name + "</div>" + "\n" + "<div class='cuisines'>" + res[j].restaurant.cuisines + "</div>");
+ 
+      }
+    },
+    error: function() {
+      $('.results').append("<div>Sorry, data is not coming through. Refresh and try again.</div>");
+    }
   });
-      
-var slider = document.getElementById('test-slider');
-noUiSlider.create(slider, {
- start: [20, 80],
- connect: true,
- step: 1,
- orientation: 'horizontal', // 'horizontal' or 'vertical'
- range: {
-   'min': 0,
-   'max': 100
- },
- format: wNumb({
-   decimals: 0
- })
-});
-
-
